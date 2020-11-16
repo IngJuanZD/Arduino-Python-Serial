5308faf1a15456aad9d93750afdaeba097ab8a1d
Juan Carlos Zúñiga Domínguez <38577604+IngJuanZD@users.noreply.github.com>
2020-11-15 21:29:18 -0600

    Puerto Serie para uso en  Linux
    Se agregan lineas de comando para soporte de puerto serie en linux y por el moenteo queda funcionado al 100% en linux
    Faltaría revisar el problema de detección de teclas presionadas virtualmente en Windows

--- PortSerial-Python.py
+++ PortSerial-Python.py
@@ -1,13 +1,16 @@
 import serial
 import pyautogui
-arduino = serial.Serial('COM9', baudrate=115200, timeout=1.0)
+#Para Windwos usar:
+    #arduino = serial.Serial('COM9', baudrate=115200, timeout=1.0)
+#Revisar fallo de decteccionde tecleado 
+arduino = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1.0)
 while True:
     dato = arduino.read().decode('ascii')    
     if (dato == 'F'):
         print('Estado F')        
-        pyautogui.keyDown('alt')
+        pyautogui.keyDown('ctrl')
         pyautogui.keyDown('down')
         pyautogui.keyUp('down')
-        pyautogui.keyUp('alt')
+        pyautogui.keyUp('ctrl')
     elif (dato == 'E'):
-        print('Estado E')        
+        print('Estado E')
